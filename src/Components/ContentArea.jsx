965b519336f9427521b5722fd5cf802f595b78d0
Neethu Sanal Kumar <117423137+Neethusanal@users.noreply.github.com>
2024-12-10 11:26:49 +0300

    tabulator.js

--- src/Components/ContentArea.jsx
+++ src/Components/ContentArea.jsx
@@ -1,34 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { ReactTabulator } from 'react-tabulator';
+import 'react-tabulator/lib/styles.css'; 
+import 'react-tabulator/lib/css/tabulator.min.css'; 
 
+//Column
+
+const columns = [
+    { title: 'TaskID', field: 'id',width: 100 },
+    { title: 'Title', field: 'Title',editor: 'input' },
+    { title: 'Description', field: 'Description' ,editor: 'input'},
+    { title: 'Status',field:'Status',editor:'Select',editorParams:{
+        values: ['To Do', 'In Progress', 'Done'], 
+      },}
+
+  ];
 const ContentArea = () => {
   const [data, setData] = useState([]);
-
+// Fetching Data from API
   const getData = async () => {
     try {
       const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
       // Directly access response.data
       setData(response.data);
+     
     } catch (error) {
       console.error('Error fetching data:', error);
     }
+   
   };
 
   useEffect(() => {
     getData();
+    console.log(data)
   }, []);
 
   return (
     <div>
-      <h1>Content Area</h1>
-      <ul>
-        {/* Render the fetched data */}
-        {data.map((item) => (
-          <li key={item.id}>
-            {item.title} - {item.completed ? 'Done' : 'To Do'}
-          </li>
-        ))}
-      </ul>
+      Task Manager
+      <ReactTabulator data={data} columns={columns} />
     </div>
   );
 };
